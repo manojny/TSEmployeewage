f257c0c448218e6ba22170b5beaf926a025ea774
manojny <manojmanuny1999@gmail.com>
2023-12-19 19:40:51 +0530

    Addes:Employee is present or absent

--- src/com/bridgelabz/Employeewage.java
+++ src/com/bridgelabz/Employeewage.java
@@ -1,10 +1,23 @@
 package com.bridgelabz;
 
+import java.util.Random;
+
 public class Employeewage {
 
 	public static void main(String[] args) {
            
-		System.out.println("Welcome to Employee Wage");
+		int isPresent=1;
+		
+		Random random = new Random();
+		
+		int randomNumber = random.nextInt(2);
+		
+		if(randomNumber == isPresent) {
+			System.out.println("Employee is present");
+		}
+		else {
+			System.out.println("Employee is absent");
+		}
 
 	}
 
