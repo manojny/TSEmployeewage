10da5788608a3d4e2c05c46b4803273652d83fd8
manojny <manojmanuny1999@gmail.com>
2023-12-19 19:34:22 +0530

    Added:Welcome message

--- src/com/bridgelabz/Employeewage.java
+++ src/com/bridgelabz/Employeewage.java
@@ -1,23 +1,10 @@
 package com.bridgelabz;
 
-import java.util.Random;
-
 public class Employeewage {
 
 	public static void main(String[] args) {
            
-		int isPresent=1;
-		
-		Random random = new Random();
-		
-		int randomNumber = random.nextInt(2);
-		
-		if(randomNumber == isPresent) {
-			System.out.println("Employee is present");
-		}
-		else {
-			System.out.println("Employee is absent");
-		}
+		System.out.println("Welcome to Employee Wage");
 
 	}
 
